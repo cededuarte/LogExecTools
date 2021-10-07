5f91d712a5a09fdfa6048d841ccbf5d504dd8b08
cededuarte <70069755+cededuarte@users.noreply.github.com>
2021-10-07 08:26:33 +0800

    added plus minus buttons

--- assets/straightforecast.js
+++ assets/straightforecast.js
@@ -1,156 +1,57 @@
-// var splitName = document.getElementById("splitName");
-// splitName.onclick = function() {
-//   document.getElementById("result").innerHTML = '';
-//   var value = document.getElementById("fullName").value;
-//   value.split('\n').forEach(fullname => {
-//    /*  console.log(fullname); */
-
-//     var spaceIndex = fullname.indexOf(" ");
-//     var firstname;
-//     var lastname;
-//     if (spaceIndex == -1) {
-//       lastname = fullname;
-//       lastname = "";
-//     } else {
-//       firstname = fullname.substring(0, spaceIndex);
-//       lastname = fullname.substr(spaceIndex + 1);       
-//     }
-
-// 			document.getElementById("result").innerHTML += lastname + ", " + firstname[0];
-
-  
-//   });
-// };
-
-
-
-var input1 = document.getElementById('input1');
-var input2 = document.getElementById('input2');
-var input3 = document.getElementById('input3');
-var input4= document.getElementById('input4');
-var input5= document.getElementById('input5');
-var input6= document.getElementById('input6');
-
+$(document).ready(function() {
+    var max_fields      = 10; //maximum input boxes allowed
+    var wrapper         = $(".input_fields_wrap"); //Fields wrapper
+    var add_button      = $(".add_field_button"); //Add button ID
+    
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e){ //on add input button click
+        e.preventDefault();
+        if(x < max_fields){ //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div><input placeholder="Last Name, F" type="text" name="mytext[]"/><a href="#" class="remove_field"><i class="fas fa-minus-circle fa-lg mx-4 data-toggle="tooltip" data-placement="top" title="Remove input field""></i></a></div>'); //add input box
+        }
+    });
+    
+    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+});
 
 
 function straightForecast() {
-var button = document.getElementById('outcome');
-
-   var str1 = "1. "+ input1.value +"/2. "+ input2.value +"<br>";
-   var str2 = "1. "+ input1.value+"/2. "+ input3.value+"<br>";     
-   var str3 = "1. "+input1.value +"/2. "+ input4.value+"<br>";    
-   var str4 = "1. "+input1.value +"/2. "+ input5.value+"<br>";    
-   var str5 = "1. "+input1.value +"/2. "+ input6.value+"<br>";  
+   let result = '';
+   const inputs = document.querySelectorAll("input");
+   for (let i = 0; i < inputs.length; i++) {
+       for (let j = 0; j < inputs.length; j++) {
+           if (i === j) continue;
+           result += '1. ' + inputs[i].value + ' / 2. ' + inputs[j].value + '<br>';
+       }
+   }
    
-   var str6 = "1. "+input2.value +"/2. "+ input1.value+"<br>";    
-   var str7 = "1. "+input2.value +"/2. "+ input3.value+"<br>"; 
-   var str8 = "1. "+input2.value +"/2. "+ input4.value+"<br>";   
-   var str9= "1. "+input2.value +"/2. "+ input5.value+"<br>";
-   var str10 = "1. "+input2.value +"/2. "+ input6.value+"<br>";
-
-   var str11 = "1. "+input3.value +"/2. "+ input1.value+"<br>";    
-   var str12 = "1. "+input3.value +"/2. "+ input2.value+"<br>"; 
-   var str13 = "1. "+input3.value +"/2. "+ input4.value+"<br>";   
-   var str14 = "1. "+input3.value +"/2. "+ input5.value+"<br>";
-   var str15 = "1. "+input3.value +"/2. "+ input6.value+"<br>";
-
-   var str16 = "1. "+input4.value +"/2. "+ input1.value+"<br>";    
-   var str17 = "1. "+input4.value +"/2. "+ input2.value+"<br>"; 
-   var str18 = "1. "+input4.value +"/2. "+ input3.value+"<br>";   
-   var str19 = "1. "+input4.value +"/2. "+ input5.value+"<br>";
-   var str20 = "1. "+input4.value +"/2. "+ input6.value+"<br>";
-
-   var str21 = "1. "+input5.value +"/2. "+ input1.value+"<br>";    
-   var str22 = "1. "+input5.value +"/2. "+ input2.value+"<br>"; 
-   var str23 = "1. "+input5.value +"/2. "+ input3.value+"<br>";   
-   var str24 = "1. "+input5.value +"/2. "+ input4.value+"<br>";
-   var str25 = "1. "+input5.value +"/2. "+ input6.value+"<br>";
-
-   var str26 = "1. "+input6.value +"/2. "+ input1.value+"<br>";    
-   var str27 = "1. "+input6.value +"/2. "+ input2.value+"<br>"; 
-   var str28 = "1. "+input6.value +"/2. "+ input3.value+"<br>";   
-   var str29 = "1. "+input6.value +"/2. "+ input4.value+"<br>";
-   var str30 = "1. "+input6.value +"/2. "+ input5.value+"<br>";
- 
-   var results1 = str1+
-                  str2+
-                  str3+
-                  str4+
-                  str5+
-                  str6+
-                  str7+
-                  str8+
-                  str9+
-                  str10+
-                  str11+
-                  str12+
-                  str13+
-                  str14+
-                  str15+
-                  str16+
-                  str17+
-                  str18+
-                  str19+
-                  str20+
-                  str21+
-                  str22+
-                  str23+
-                  str24+
-                  str25+
-                  str26+
-                  str27+
-                  str28+
-                  str29+
-                  str30;
-
-      
-    document.getElementById('outcome').innerHTML=results1;
-    
-
+   document.getElementById('outcome').innerHTML = result; 
 }
+
+
 
 function enhancedDouble() {
-var button = document.getElementById('outcome');
-        var str31 = input2.value +"/"+ input1.value+"<br>";
-        var str32 = input3.value +"/"+ input1.value+"<br>";
-        var str33 = input4.value +"/"+ input1.value+"<br>";
-        var str34 = input5.value +"/"+ input1.value+"<br>";
-        var str35 = input6.value +"/"+ input1.value+"<br>";
-        
-        var str36 = input3.value +"/"+ input2.value+"<br>";
-        var str37 = input4.value +"/"+ input2.value+"<br>";
-        var str38 = input5.value +"/"+ input2.value+"<br>";
-        var str39 = input6.value +"/"+ input2.value+"<br>";     
- 
-        var str40 = input4.value +"/"+ input3.value+"<br>";
-        var str41 = input5.value +"/"+ input3.value+"<br>";
-        var str42 = input6.value +"/"+ input3.value+"<br>";  
-
-        var str43 = input5.value +"/"+ input4.value+"<br>";
-        var str44 = input6.value +"/"+ input4.value+"<br>";
-        
-        var str45 = input6.value +"/"+ input5.value+"<br>";  
-
-
-			var results2 = str31+
-                    str32+
-                    str33+
-                    str34+
-                    str35+
-                    str36+
-                    str37+
-                    str38+
-                    str39+
-                    str40+
-                    str41+
-                    str42+
-                    str43+
-                    str44+
-                    str45;
-
-		   document.getElementById('outcome').innerHTML=results2;
-
+   let result2 = '';
+   const inputs = document.querySelectorAll("input");
+   for (let i = 0; i < inputs.length; i++) {
+       for (let j = i+1; j < inputs.length; j++) {
+         
+           result2 += inputs[i].value + '/' + inputs[j].value + '<br>';
+           
+       }
+   }
+   
+   document.getElementById('outcome').innerHTML = result2; 
 }
+
+
+
+
+
+
 
 
   function copyToClipboard(id) {
